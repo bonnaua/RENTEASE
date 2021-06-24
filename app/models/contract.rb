bdb43bf04ae89516091f9af09a31ca05a72e8aa3
Audrey Gonera <audrey.gonera@gmail.com>
2021-06-24 18:34:09 +0200

    index done, style needed

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -2,6 +2,7 @@ class Contract < ApplicationRecord
   belongs_to :housing
   has_many :rents
   has_many :renter_contracts
+  has_many :renters, through: :renter_contracts
 
   validates :start_date, :end_date, :rent_due_date, :rent, :rental_expenses, presence: true
 end
