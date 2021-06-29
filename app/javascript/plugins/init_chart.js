76ccd30a13ee02ca1f34b65594eda1667878bcd2
aymeric bonnaud <aymeric.bonnaud@edhec.com>
2021-06-29 11:29:18 +0200

    quickfix

---
+++ app/javascript/plugins/init_chart.js
@@ -0,0 +1,130 @@
+import {
+  Chart,
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip
+} from 'chart.js';
+
+Chart.register(
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip
+);
+
+const initChartJS =  () => {
+  const dataSet = document.querySelector('.main-dashboard').dataset
+  const expenses = {}
+  const rents = JSON.parse(dataSet.rents)
+  const profits = {}
+  const dataExpenses = JSON.parse(dataSet.expenses)
+  const tresuryLine = {}
+  Object.keys(dataExpenses).forEach((key) => {
+    const value = dataExpenses[key]
+    const newKey = key.split(" ")[0]
+    expenses[newKey] = value
+  })
+
+  Object.keys(expenses).forEach((month, index) => { // Postulat que expenses continues (tous les mois)
+    const rent = (rents[month] === undefined) ? 0 : rents[month] // Si pas de rents
+    profits[month] = rent - expenses[month]
+    tresuryLine[month] = Object.values(profits).reduce((a, b) => a + b, 0)
+    console.log(tresuryLine)
+  })
+
+
+
+
+  const myChart = new Chart(document.getElementById("bar-chart"), {
+
+    data: {
+      labels: Object.keys(profits),
+      datasets: [
+        {
+          type: 'bar',
+          label: "Profit (€)",
+          backgroundColor: "#DAF552",
+          yAxisID: 'y',
+          data: Object.values(profits)
+        },
+        {
+          type: 'line',
+          label: 'Tresory Line',
+          yAxisID: 'y1',
+          data: Object.values(tresuryLine)
+        }
+      ]
+    },
+    options: {
+      plugins: {
+        legend: { display: true },
+        title: {
+          display: true,
+          text: 'Profit over the last year'
+        }
+      },
+
+      scales: {
+        x: {
+          grid: {
+            display: false
+          }
+        },
+        y: {
+           type: 'linear',
+           display: true,
+           position: 'left',
+         },
+        y1: {
+           type: 'linear',
+           display: true,
+           position: 'right'
+        }
+      }
+    }
+  }
+  );
+};
+
+
+
+export {initChartJS}
